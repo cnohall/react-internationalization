0238952d417b4cf0e21a32329eee2ba3c4739645
cnohall <christopher.nohall@gmail.com>
2021-06-18 20:52:12 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import LocalizedStrings from 'react-localization';
+
+function App() {
+  let strings = new LocalizedStrings(
+    {
+    "en-US":{
+      how:"How do you want your egg today?",
+      boiledEgg:"Boiled egg",
+      softBoiledEgg:"Soft-boiled egg",
+      choice:"How to choose the egg"
+    },
+    en:{
+      how:"How do you want your egg today?",
+      boiledEgg:"Boiled egg",
+      softBoiledEgg:"Soft-boiled egg",
+      choice:"How to choose the egg"
+    },
+    it: {
+      how:"Come vuoi il tuo uovo oggi?",
+      boiledEgg:"Uovo sodo",
+      softBoiledEgg:"Uovo alla coque",
+      choice:"Come scegliere l'uovo"
+    },
+    kr: {
+      how:"test1?",
+      boiledEgg:"test2",
+      softBoiledEgg:"test3",
+      choice:"test4"
+    }
+   });
+  console.log(strings.getInterfaceLanguage());
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
