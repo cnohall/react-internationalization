845efdf9b481b9608c300bee861f7d5a4eb06d60
cnohall <christopher.nohall@gmail.com>
2021-06-19 17:32:56 +0900

    remove react-localization

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import LocalizedStrings from 'react-localization';
 
 function App() {
-  let strings = new LocalizedStrings(
-    {
-    "en-US":{
-      how:"How do you want your egg today?",
-      boiledEgg:"Boiled egg",
-      softBoiledEgg:"Soft-boiled egg",
-      choice:"How to choose the egg"
-    },
-    en:{
-      how:"How do you want your egg today?",
-      boiledEgg:"Boiled egg",
-      softBoiledEgg:"Soft-boiled egg",
-      choice:"How to choose the egg"
-    },
-    it: {
-      how:"Come vuoi il tuo uovo oggi?",
-      boiledEgg:"Uovo sodo",
-      softBoiledEgg:"Uovo alla coque",
-      choice:"Come scegliere l'uovo"
-    },
-    kr: {
-      how:"test1?",
-      boiledEgg:"test2",
-      softBoiledEgg:"test3",
-      choice:"test4"
-    }
-   });
-  console.log(strings.getInterfaceLanguage());
   return (
     <div className="App">
       <header className="App-header">
