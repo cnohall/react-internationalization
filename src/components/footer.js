eaaf7c040c86fbf894e9cd7ea0ef084df05bf01a
cnohall <christopher.nohall@gmail.com>
2021-06-19 19:58:50 +0900

    remove console.log

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,11 +5,6 @@ const getGreetingTime = (d = DateTime.now()) => {
     const split_afternoon = 12; // 24hr time to split the afternoon
     const split_evening = 17; // 24hr time to split the evening
     const currentHour = d.c.hour;
-
-    console.log(currentHour);
-
-
-
     if (currentHour >= split_afternoon && currentHour <= split_evening) {
         return 'afternoon';
     } else if (currentHour >= split_evening) {
