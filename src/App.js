bd6dbb4eebfc37811b23437edb3dc7c3d0f3ecc4
cnohall <christopher.nohall@gmail.com>
2021-06-19 19:41:22 +0900

    add suspense in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useTranslation, Trans } from 'react-i18next';
-import { useState } from 'react';
+import { useState, Suspense  } from 'react';
 import Footer from './components/footer';
 
 const lngs = {
@@ -14,38 +14,40 @@ function App() {
   const [count, setCounter] = useState(0);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          {Object.keys(lngs).map((lng) => (
-            <button key={lng} style={{ fontWeight: i18n.language === lng ? 'bold' : 'normal' }} type="submit" onClick={() => {
-              i18n.changeLanguage(lng);
-              setCounter(count + 1);
-            }}>
-              {lngs[lng].nativeName}
-            </button>
-          ))}
-        </div>
-        <p>
-          <i>{t('counter', { count })}</i>
-        </p>
-        <p>
-          <Trans i18nKey="description.part1">
-            Edit <code>src/App.js</code> and save to reload.
-          </Trans>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {t('description.part2')}
-        </a>
-      </header>
-      <Footer t={t} />
-    </div>
+    <Suspense fallback="...is loading">
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div>
+            {Object.keys(lngs).map((lng) => (
+              <button key={lng} style={{ fontWeight: i18n.language === lng ? 'bold' : 'normal' }} type="submit" onClick={() => {
+                i18n.changeLanguage(lng);
+                setCounter(count + 1);
+              }}>
+                {lngs[lng].nativeName}
+              </button>
+            ))}
+          </div>
+          <p>
+            <i>{t('counter', { count })}</i>
+          </p>
+          <p>
+            <Trans i18nKey="description.part1">
+              Edit <code>src/App.js</code> and save to reload.
+            </Trans>
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {t('description.part2')}
+          </a>
+        </header>
+        <Footer t={t} />
+      </div>
+    </Suspense>
   );
 }
 
